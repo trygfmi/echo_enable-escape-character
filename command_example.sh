bc38a02295e0718c811ae4de8c862d3fd5e3fa6c
terner-running-terminal-commands <runningterminalcommands@gmail.com>
2025-12-28 17:28:40 +0900

    first dev commit

---
+++ command_example.sh
@@ -0,0 +1,12 @@
+#"!/bin/bash
+# ./command_example.sh
+
+
+# file_absolute_path="$(readlink -f "$0")"
+# echo "$file_absolute_path"
+
+
+echo "apple\nbanana\ncherry"
+echo -e "apple\nbanana\ncherry"
+echo -E "apple\nbanana\ncherry"
+
